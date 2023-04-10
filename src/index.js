7ddce792c6c27dcc8a5d64ab8b89fcf824f46930
Oumayma Mahfoudhi <92217015+OumaymaMahfoudhi@users.noreply.github.com>
2023-04-11 00:25:42 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,24 +5,6 @@ import {BrowserRouter} from 'react-router-dom'
 import Upload from './views/dashboard/Upload';
 
 
-
-
-import Upload from './views/dashboard/Upload';
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
