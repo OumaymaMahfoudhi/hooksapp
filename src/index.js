273adb2c265eb1f852a0fbf28a41e7105f89f88f
Oumayma Mahfoudhi <92217015+OumaymaMahfoudhi@users.noreply.github.com>
2023-04-11 00:25:11 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,20 @@ import Upload from './views/dashboard/Upload';
 
 
 
+
+import Upload from './views/dashboard/Upload';
+import Upload from './views/dashboard/Upload';
+import Upload from './views/dashboard/Upload';
+
+
+
+
+
+
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
