2c13f3f575b5f065143608764c7aa5bdc680479e
Oumayma Mahfoudhi <92217015+OumaymaMahfoudhi@users.noreply.github.com>
2023-04-25 18:34:59 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
-
-
-
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {BrowserRouter} from 'react-router-dom'
 import Upload from './views/dashboard/Upload';
-
-
-
-//import React from 'react';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
